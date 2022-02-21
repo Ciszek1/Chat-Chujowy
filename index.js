6f74f3087c7fd6292d87b3da460779f3c24ffc6b
Ciszek1 <61918655+Ciszek1@users.noreply.github.com>
2022-02-21 22:21:44 +0100

    wwww

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/newmsg',cors(), (req, res) => {
 
   json = JSON.parse(fs.readFileSync("room/"+roomID+".json",{encoding:'utf8', flag:'r'}))
   json.push({"nick":nick,"msg":msg,"date":nowDate})
-  fs.writeFileSync("room/"+roomID+".json",json)
+  fs.writeFileSync("room/"+roomID+".json",json, {flag:'w'})
   newJson = fs.readFileSync("room/"+roomID+".json",{encoding:'utf8', flag:'r'})
   res.contentType('application/json');
   res.send(newJson)
