2496a4fed507b81f7e5f41ade99e823dc453ac77
Ciszek1 <61918655+Ciszek1@users.noreply.github.com>
2022-02-21 01:56:59 +0100

    swa

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/newmessage',cors(), (req, res) => {
-  const value = req.query.value;
+  var value = req.query.value;
   console.log(value)
   fs.open("data.txt", "a", (err, fd)=>{
     console.log("fd")
